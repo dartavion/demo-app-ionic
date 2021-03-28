79077c9a1da92ca7494eb071dab08aaf2e0d5ed5
Richard Ranke <richardranke@stantoninnovations.com>
2021-03-28 15:51:29 -0400

    feat: setup staging

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExampleService } from './services/example.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, ExampleService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ExampleService } from '../services/example.service';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,15 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  api;
+  environment;
+
+  example$ = this.exampleService.get();
+  constructor(private exampleService: ExampleService) {
+    this.api = environment.api;
+    this.environment = environment.name;
+    console.log(this.api);
+    console.log(this.environment);
+  }
 
 }
